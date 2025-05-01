3fa2626bf08e5b1fba0ea5f4b8eed463932b9827
Arnav Verma <arnavverma3016@gmail.com>
2025-05-01 18:18:36 +0530

    Get_Request

--- backend/Controllers/UserControllers.js
+++ backend/Controllers/UserControllers.js
@@ -1,12 +1,13 @@
 const User = require('../Models/User');
 
+//create user
 const createUser = async (req, res) => {
   try {
     const { name, email, password, age, profession, bio , isVerified} = req.body;
 
     // Validate required fields
     if (!name || !email || !password || !age) {
-      return res.status(400).json({ error: 'All fields are required are required.' });
+      return res.status(400).json({ error: 'All fields are required .' });
     }
 
     const user = await User.create({
@@ -30,4 +31,28 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser };
+// get all users
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//get userbyID
+const getUserById = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = { createUser , getAllUsers , getUserById };
