878470840ec1dfd4796e307d99ce23a2795a2037
Arnav Verma <arnavverma3016@gmail.com>
2025-05-09 18:10:11 +0530

    Creating_Components

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,10 +1,11 @@
-
+import React from 'react';
+import Register from './components/Register';
 function App() {
-  return(
+  return (
     <div>
-      <h1>Hello World</h1>
+      <Register />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
